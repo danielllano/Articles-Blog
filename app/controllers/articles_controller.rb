3d1fefdd660623f99d67cf3ed22049e6239c0030
Daniel Llano <daniel.llano.a@gmail.com>
2014-12-12 10:33:31 -0500

    Added modal to new and edit articles, and set sidebar elements as links to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to articles_path
     else
       render 'new'
     end
