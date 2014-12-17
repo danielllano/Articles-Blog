b73ae0e55e448970175c2c8c6d32d02dce9afbef
Daniel Llano <daniel.llano.a@gmail.com>
2014-12-17 16:53:52 -0500

    Associated comments to users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
     redirect_to article_path(@article)
   end
 
